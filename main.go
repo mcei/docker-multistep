5b59caadbecb71ff3b96f8bc1fa260f5a17121b0
mcei <mcwave@ya.ru>
2021-12-01 02:30:57 +0700

    go fmt

--- main.go
+++ main.go
@@ -1,23 +1,22 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "log"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 func main() {
-    resp, err := http.Get("https://google.com")
+	resp, err := http.Get("https://google.com")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-    body, err := ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-    fmt.Println(len(body))
+	fmt.Println(len(body))
 }
-
