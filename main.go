92e956bad6c8a985c279398542d404b97163baad
mcei <mcwave@ya.ru>
2021-12-01 02:24:09 +0700

    Go fetches content from page

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "log"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+    resp, err := http.Get("https://google.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+    body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+    fmt.Println(len(body))
 }
+
